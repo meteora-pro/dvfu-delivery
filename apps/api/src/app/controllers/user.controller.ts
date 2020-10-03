20021512b649fa9769d55ba7824a785c949b32c5
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-04 01:52:16 +0300

    Disable user crud endpoints

--- apps/api/src/app/controllers/user.controller.ts
+++ apps/api/src/app/controllers/user.controller.ts
@@ -5,14 +5,14 @@ import { UserEntity } from '../entities/user.entity';
 import { UserService } from '../services/user.service';
 
 @ApiTags('user')
-@Crud({
-  model: {
-    type: UserEntity,
-  },
-  routes: {
-    only: ['getOneBase', 'createOneBase'],
-  },
-})
+// @Crud({
+//   model: {
+//     type: UserEntity,
+//   },
+//   routes: {
+//     only: ['getOneBase', 'createOneBase'],
+//   },
+// })
 @Controller("users")
 export class UserController implements CrudController<UserEntity> {
   constructor(public service: UserService) {}
