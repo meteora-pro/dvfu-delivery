4865d21906cdbbdef5a4ce250822a3cdd5784864
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-03 06:34:40 +0300

    Add material, fix npm start script

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    MatButtonModule,
     HttpClientModule
   ],
   providers: [],
