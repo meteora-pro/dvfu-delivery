16b449c17808e69f8e0cce3cd1fccd4d9ed1da53
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-03 21:48:23 +0300

    Fix readme, fix order detail

--- apps/frontend/src/app/delivery/order-detail/order-detail.component.ts
+++ apps/frontend/src/app/delivery/order-detail/order-detail.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Order } from '@dvfu-delivery/types';
+import { of } from 'rxjs';
 
 @Component({
-  selector: 'dvfu-delivery-order-detail',
+  selector: 'delivery-order-detail',
   templateUrl: './order-detail.component.html',
   styleUrls: ['./order-detail.component.scss']
 })
 export class OrderDetailComponent implements OnInit {
+
+  @Input() order: Order;
+
+  productPlural = {
+    zero: "",
+    one: "# товар",
+    few: "# товара",
+    many: "# товаров",
+    other: "# товаров",
+  };
+  currentUser$ = of(null);
 
   constructor() { }
 
