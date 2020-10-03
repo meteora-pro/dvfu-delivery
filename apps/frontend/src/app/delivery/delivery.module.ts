dacafe7aa935ba99902993f6ec836ac991e3c997
Filipp Semirov <f.semirov@cm.expert>
2020-10-03 13:23:09 +0300

    implement delivery list component

--- apps/frontend/src/app/delivery/delivery.module.ts
+++ apps/frontend/src/app/delivery/delivery.module.ts
@@ -4,9 +4,10 @@ import { DeliveryComponent } from './delivery.component';
 import { RouterModule } from '@angular/router';
 import { CreateDeliveryComponent } from './create-delivery/create-delivery.component';
 import { SharedModule } from '../shared/shared.module';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 @NgModule({
-  declarations: [DeliveryComponent, CreateDeliveryComponent],
+  declarations: [DeliveryComponent, CreateDeliveryComponent, OrderDetailComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
@@ -17,6 +18,10 @@ import { SharedModule } from '../shared/shared.module';
           {
             path: 'create',
             component: CreateDeliveryComponent
+          },
+          {
+            path: 'detail/:id',
+            component: OrderDetailComponent
           }
         ]
       },
---
+++ apps/frontend/src/app/delivery/order-detail/order-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dvfu-delivery-order-detail',
+  templateUrl: './order-detail.component.html',
+  styleUrls: ['./order-detail.component.scss']
+})
+export class OrderDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
